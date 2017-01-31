ce9853b33e733d0a892a76f9a768697bb6ba9ca4
SungHo Hong <maverickjin88@gmail.com>
2017-01-30 18:44:17 -0700

    added list features

--- modals/Attendant.py
+++ modals/Attendant.py
@@ -1,24 +1,85 @@
-from google.appengine.ext import db
+from google.appengine.ext import ndb
+from google.appengine.datastore.datastore_query import Cursor
 
 
-class Attendant(db.Model):
-    firstName = db.StringProperty('FirstName')
-    lastName = db.StringProperty('LastName')
-    email = db.StringProperty('Email')
-    dietaryPreferences = db.StringProperty('DietaryPreferences')
-    specialAccomodations = db.StringProperty('SpecialAccomodations')
-    present = db.BooleanProperty('Present', default=False)
+class Attendant(ndb.Model):
+    firstName = ndb.StringProperty('FirstName')
+    lastName = ndb.StringProperty('LastName')
+    email = ndb.StringProperty('Email')
 
-def generateTestData():
-    a = Attendant(firstName='Steve', lastName='King', email='sking11@asu.edu',
-                dietaryPreferences='None', specialAccomodations='')
-    a.put()
-    a = Attendant(firstName='Ryan', lastName='Ang', email='rang1@asu.edu',
-                    dietaryPreferences='Vegan', specialAccomodations='Pls no')
-    a.put()
-    a = Attendant(firstName='SungHo', lastName='Hong', email='sungho@asu.edu',
-                    dietaryPreferences='None', specialAccomodations='')
-    a.put()
-    a = Attendant(firstName='Darrell', lastName='Jackson', email='bigdaddy@asu.edu',
-                    dietaryPreferences='Pizza', specialAccomodations='Call me big daddy')
-    a.put()
+    '''
+        Using lowercase is also impossible in Google App Engine
+    '''
+    firstName_lower = ndb.ComputedProperty(lambda self: self.firstName.lower())
+    lastName_lower = ndb.ComputedProperty(lambda self: self.lastName.lower())
+    email_lower = ndb.ComputedProperty(lambda self: self.email.lower())
+
+    dietaryPreferences = ndb.StringProperty('DietaryPreferences')
+    specialAccomodations = ndb.StringProperty('SpecialAccomodations')
+    present = ndb.BooleanProperty('Present', default=True)
+    created = ndb.DateTimeProperty(auto_now_add=True)
+
+    @classmethod
+    def cursor_pagination(cls, prev_cursor_str, next_cursor_str, total_page, search_dict=False):
+
+
+        '''
+            Currently it is impossible for me to create paging while searching
+            Because the Google App Engine uses cursors and ordering the rows are important for paging
+            While when we use operands for searching -> ordering is prohibited
+        '''
+
+        if search_dict:
+            limit = search_dict['text'][:-1] + chr(ord(search_dict['text'][-1]) + 1)
+            for property, value in vars(cls).iteritems():
+                if search_dict['col_name'] is property:
+                    col_name = value
+            query_start = cls.query(col_name >= search_dict['text'].lower(), col_name < limit)
+
+            # query_start = cls.query(ndb.OR(cls.firstName_lower >= search_dict['text'].lower(), cls.lastName_lower >= search_dict['text'].lower(),
+            #                                   cls.email_lower >= search_dict['text'].lower()))
+        else:
+            if prev_cursor_str:
+                query_start = cls.query().order(-cls.created)
+            else:
+                query_start = cls.query().order(cls.created)
+
+
+        if not prev_cursor_str and not next_cursor_str:
+            objects, next_cursor, more = query_start.fetch_page(total_page)
+            prev_cursor_str = ''
+            if next_cursor:
+                next_cursor_str = next_cursor.urlsafe()
+            else:
+                next_cursor_str = ''
+            next_ = True if more else False
+            prev = False
+        elif next_cursor_str:
+            cursor = Cursor(urlsafe=next_cursor_str)
+            objects, next_cursor, more = query_start.fetch_page(total_page, start_cursor=cursor)
+            prev_cursor_str = next_cursor_str
+            next_cursor_str = next_cursor.urlsafe()
+            prev = True
+            next_ = True if more else False
+        elif prev_cursor_str:
+            cursor = Cursor(urlsafe=prev_cursor_str)
+            objects, next_cursor, more = query_start.fetch_page(total_page, start_cursor=cursor)
+            objects.reverse()
+            next_cursor_str = prev_cursor_str
+            prev_cursor_str = next_cursor.urlsafe()
+            prev = True if more else False
+            next_ = True
+
+        objects = [{
+            'id': attendant.put().urlsafe(),
+            'first_name': attendant.firstName,
+            'last_name': attendant.lastName,
+            'email': attendant.email,
+            'dietary_preferences': attendant.dietaryPreferences,
+            'special_accomodations': attendant.specialAccomodations,
+            'present': attendant.present
+        } for attendant in objects]
+
+        return {'objects': objects, 'next_cursor': next_cursor_str, 'prev_cursor': prev_cursor_str, 'prev': prev, 'next': next_}
+
+
--- views/CheckinListHandler.py
+++ views/CheckinListHandler.py
@@ -2,9 +2,14 @@ from google.appengine.ext.webapp import template
 import webapp2, os
 from google.appengine.api import users
 from modals.Attendant import Attendant
-from google.appengine.ext.db import Key
 import json
 
+
+col_names = {
+    'firstName': 'firstName_lower',
+    'lastName': 'lastName_lower',
+    'email': 'email_lower'
+}
 
 class CheckinListHandler(webapp2.RequestHandler):
     def get(self):
@@ -13,24 +18,45 @@ class CheckinListHandler(webapp2.RequestHandler):
             1. make the pagination work based on the right google app engine protocal
             2. use the url paging data to track the page even if the page is refreshed
                ex) if you refresh your webpage on page 2, the refreshed data should remain in page 2
+
+            3. search data - check
+            4. total page check - check
+            5. POST type change
+            6. toggle update dummy add
+                - simple check update
         '''
 
-
-        attendant_list = [{
-            'id': attendant.put().id(),
-            'first_name': attendant.firstName,
-            'last_name': attendant.lastName,
-            'email': attendant.email,
-            'dietary_preferences': attendant.dietaryPreferences,
-            'special_accomodations': attendant.specialAccomodations,
-            'present': attendant.present
-        } for attendant in Attendant.all()]
-
+        prev_cursor = self.request.get('prev_cursor', '')
+        next_cursor = self.request.get('next_cursor', '')
+        attendant_list = Attendant.cursor_pagination(prev_cursor, next_cursor, 3)
 
         template_values = {
             'logout_url': users.create_logout_url('/'),
-            'attendant_list': json.dumps(attendant_list)
+            'attendant_list': json.dumps(attendant_list['objects']),
+            'next_cursor': attendant_list['next_cursor'],
+            'prev_cursor' : attendant_list['prev_cursor'],
+            'prev': attendant_list['prev'],
+            'next': attendant_list['next'],
         }
 
         path = os.path.join(os.path.dirname(__file__), '../templates/checkin_list.html')
         self.response.out.write(template.render(path, template_values))
+
+
+    def post(self):
+        text = str(self.request.get('text'))
+        col_name = col_names[str(self.request.get('col_name'))]
+
+        attendant_list = Attendant.cursor_pagination('', '', Attendant.query().count(),
+                         {'col_name': col_name, 'text': text.lower()})
+        json_return = {
+            'attendant_list': json.dumps(attendant_list['objects']),
+        }
+
+        json_return = json.dumps(json_return)
+        self.response.headers['Content-Type'] = 'application/json; charset=UTF-8'
+        self.response.out.write(json_return)
+
+
+
+
