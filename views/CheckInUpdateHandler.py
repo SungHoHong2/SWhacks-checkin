820a4e8283a83a7399b372fadca7bcac40f8efc4
Darrell Jackson <desertcountryside@gmail.com>
2017-01-31 17:10:22 -0700

    working update for one user

--- views/CheckInUpdateHandler.py
+++ views/CheckInUpdateHandler.py
@@ -2,12 +2,12 @@ from google.appengine.ext.webapp import template
 import webapp2, os
 from google.appengine.api import users
 from modals.Attendant import Attendant
+from google.appengine.ext import ndb
 import json
-from google.appengine.ext.db import Key
+
 
 class CheckInUpdateHandler(webapp2.RequestHandler):
     def post(self):
-
 
         '''
             1. update the present column to True or False
@@ -16,20 +16,41 @@ class CheckInUpdateHandler(webapp2.RequestHandler):
             3. if update is unsuccessful then return the success value False
         '''
 
-        is_present = self.request.get('is_present')
-        _id = self.request.get('keys')
+        # extract self.request.get("keys") into an array and loop through all of them
+        # 1. for each item, get the key
+        # 2. Take key and get attendant from it
+        # 3. invert the attendant's present boolean
+        # 4. put into database
+        # 5. make a copy of the key, and place into an array called "_id"
 
-        print _id
+
+
+
+        # This works for one user.
+        key_str = self.request.get("keys").split(',')[0]
+
+        key = ndb.Key(urlsafe=key_str)
+        attendant = key.get()
+
+        is_present = attendant.present
+        # If the attendant is present, mark him/her not present
+        # We do this inversion because a staff member might have checked the wrong person
+        if is_present:
+            attendant.present = False
+        else:
+            attendant.present = True
+
+        attendant.put()
+
+        _id = self.request.get("keys").split(',')[0]
+        # this works for one user
 
         if is_present:
             print 'change present to false'
         else:
             print 'change present to true'
 
-
-        response_data = {"success": True, 'keys': _id[0:len(_id)-1]}
+        response_data = {"success": True, 'id': _id}
         json_rtn = json.dumps(response_data)
         self.response.headers['Content-Type'] = 'application/json; charset=UTF-8'
         self.response.out.write(json_rtn)
-
-
