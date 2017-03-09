58bd7e797c44d47384719ebca4780cb5f6f37f95
SungHo Hong <maverickjin88@gmail.com>
2017-03-09 16:30:51 -0700

    added export csv

--- views/ExportCSVHandler.py
+++ views/ExportCSVHandler.py
@@ -1,20 +1,30 @@
 from modals.Attendant import Attendant
 from views.SessionHandler import SessionHandler
 import csv
+import urllib2, json
 
 class ExportCSVHandler(SessionHandler):
     def get(self):
 
         self.response.headers['Content-Type'] = 'application/csv'
         writer = csv.writer(self.response.out)
-        writer.writerow(["first_name\tlast_name\temail"])
 
+        req = urllib2.urlopen(
+            'https://api.typeform.com/v1/form/tPmlbe?key=c023bc0bb7371530bd64e4fd052759b41a74b93b&completed=true')
+        data = json.load(req)
+
+
+        csv_dict = {}
+        raw_data = data.get('responses')
+        for people in raw_data:
+
+            if people.get('answers').get('email_40994675') is not None:
+                csv_dict[people.get('answers').get('email_40994675')] = people.get('answers').get('textfield_40994671')
+
+
+        writer.writerow(["first_name","last_name","email","school"])
         for attendant in Attendant.query().filter(Attendant.present == True):
-            writer.writerow([attendant.firstName, attendant.lastName, attendant.email])
-
-            #print "{0}\t{1}\t{2}\n".format(attendant.firstName, attendant.lastName, attendant.email)
-            #output_aray.append("{0}\t{1}\t{2}\n".format(attendant.firstName, attendant.lastName, attendant.email))
-
-
-
-
+            writer.writerow([ attendant.firstName,
+                              attendant.lastName,
+                              attendant.email,
+                              csv_dict[attendant.email] ])
